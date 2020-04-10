7294e88aac682b8edaf0f40833d435d913007629
zacharygilliom <zacharygilliom@gmail.com>
2020-04-10 00:24:26 -0400

    added hiking trails API and added Hiking Trails webpage.

--- app.py
+++ app.py
@@ -12,18 +12,21 @@ maps_KEY = config.google_maps_key
 
 app = Flask(__name__)
 
-class UserLocation(object):
 
+class UserLocation(object):
+	''' 
+	Defines the location given by the user in latitude and longitude
+	'''
 	def __init__ (self, user_lat, user_lon):
 		self.lat = user_lat
 		self.lon = user_lon
 
 
-class Trail:
-	"""
-	This will store all of our data we get from our JSON file through the MTB get-trails
-	API.  Also will calculate distance to origin
-	"""
+class HikeTrail:
+	'''
+	This will store all of our Hike Trail data we get from our JSON file through the Hike Project get-trails
+	API.  Also a method to calculate the distance to the origin.
+	'''
 
 	def __init__(self, id, name, conditionStatus, conditionDetails, conditionDate, rating, lat, lon, difficulty):
 		self.id = id
@@ -36,32 +39,90 @@ class Trail:
 		self.lon = lon
 		self.difficulty = difficulty
 
-
-	def get_condition_keywords(self):
+	def get_hike_condition_keywords(self):
 		# Looking for keywords that might impact trail conditions.
 		# i.e. if it is muddy - then most likely it will still be muddy no matter the weather.
 		# if the conditionDetails parameter is None - it returns no condtions given.
 		keywords = ['wet', 'mud', 'snow', 'dry', 'rock']
-		trail_keywords = []
+		hike_trail_keywords = []
 		if self.conditionDetails is None:
-			trail_keywords.append('No Conditions Given')
+			hike_trail_keywords.append('No Conditions Given')
 		else:
 			for word in keywords:
 				start = self.conditionDetails.find(word)
 				if start >= 0:
-					trail_keywords.append(word)
-			if trail_keywords:
+					hike_trail_keywords.append(word)
+			if hike_trail_keywords:
 				pass
 			else:
-				trail_keywords.append('None')
+				hike_trail_keywords.append('None')
 
-		return trail_keywords
+		return hike_trail_keywords
 
-				
+	def get_distance_to_origin(self, UserLocation):
+	   # The math module contains a function named 
+	   # radians which converts from degrees to radians.
+		selected_lat = self.lat
+		selected_lon = self.lon
+		lon2 = UserLocation.lon
+		lat2 = UserLocation.lat
+		selected_lon = radians(selected_lon)
+		lon2 = radians(lon2)
+		selected_lat = radians(selected_lat)
+		lat2 = radians(lat2) 
+	
+		# Haversine formula
+		dlon = lon2 - selected_lon
+		dlat = lat2 - selected_lat
+		a = sin(dlat / 2)**2 + cos(selected_lat) * cos(lat2) * sin(dlon / 2)**2
+		c = 2 * asin(sqrt(a))
+		# Radius of earth in kilometers. Use 3956 for miles
+		r = 6371
+		kilometers = c * r
+		miles = round((kilometers * 0.62137),2)
+
+		# calculate the result
+		return miles 
+
+
+class BikeTrail:
+	'''
+	This will store all of our Bike Trail data we get from our JSON file through the MTB get-trails
+	API.  Also will calculate distance to origin
+	'''
+
+	def __init__(self, id, name, conditionStatus, conditionDetails, conditionDate, rating, lat, lon, difficulty):
+		self.id = id
+		self.name = name
+		self.conditionStatus = conditionStatus
+		self.conditionDetails = conditionDetails
+		self.conditionDate = conditionDate
+		self.rating = rating
+		self.lat = lat
+		self.lon = lon
+		self.difficulty = difficulty
+
+	def get_bike_condition_keywords(self):
+		# Looking for keywords that might impact trail conditions.
+		# i.e. if it is muddy - then most likely it will still be muddy no matter the weather.
+		# if the conditionDetails parameter is None - it returns no condtions given.
+		keywords = ['wet', 'mud', 'snow', 'dry', 'rock']
+		bike_trail_keywords = []
+		if self.conditionDetails is None:
+			bike_trail_keywords.append('No Conditions Given')
+		else:
+			for word in keywords:
+				start = self.conditionDetails.find(word)
+				if start >= 0:
+					bike_trail_keywords.append(word)
+			if bike_trail_keywords:
+				pass
+			else:
+				bike_trail_keywords.append('None')
+
+		return bike_trail_keywords
 
 	# to calculate the distance between the trail location and the location that the user entered.
-	# TODO need to fix how I get the users input location.
-
 	def get_distance_to_origin(self, UserLocation):
 	   # The math module contains a function named 
 	   # radians which converts from degrees to radians.
@@ -112,9 +173,7 @@ class YesterdayWeather:
 			return 'Dry'
 
 
-
-
-def get_trails(lat, lon, maxDistance, key):
+def get_hike_trails(lat, lon, maxDistance, key):
 	# Get all the trails within a given distance from the user.
 	# this will return a list of Instantiated Trail Objects.
 
@@ -122,19 +181,19 @@ def get_trails(lat, lon, maxDistance, key):
 	
 	#Uncomment this for live API version.  This is commented out so that we can save our API requests
 	
-	# request = requests.get(f'http://www.mtbproject.com/data/get-trails?lat={lat}&lon={lon}&maxDistance={maxDistance}&key={key}&maxResults=20')	
-	# trails_text = request.text
-	# trails_dict = json.loads(trails_text)
+	request = requests.get(f'http://www.hikingproject.com/data/get-trails?lat={lat}&lon={lon}&maxDistance={maxDistance}&key={key}&maxResults=20')	
+	trails_text = request.text
+	trails_dict = json.loads(trails_text)
 
 	# Writing our api call data to a json file for continued use instead of making api requests.  Commented out because we don't want to updated
 	# our file and because our api isn't making requsts right now.
 	
-	# with open('trailsApiData.json', 'w') as json_file:
+	# with open('trailsHikeApiData.json', 'w') as json_file:
 	# 	json.dump(trails_dict, json_file)
 	
 	# Loading in our Trail Data test so we can play around with html without making api requests.
 	
-	with open('trailsApiData.json') as f:
+	with open('trailsHikeApiData.json') as f:
 		trails_dict = json.load(f)
 
 	trails = []
@@ -154,7 +213,54 @@ def get_trails(lat, lon, maxDistance, key):
 		lat = t['latitude']
 		lon = t['longitude']
 		difficulty = t['difficulty']
-		trail = Trail(id=id, name=name, conditionStatus=conditionStatus, conditionDetails=conditionDetails, conditionDate=conditionDate,
+		trail = BikeTrail(id=id, name=name, conditionStatus=conditionStatus, conditionDetails=conditionDetails, conditionDate=conditionDate,
+					rating=rating, lat=lat, lon=lon, difficulty=difficulty)
+		trails.append(trail)
+	
+	return trails
+
+
+def get_bike_trails(lat, lon, maxDistance, key):
+	# Get all the trails within a given distance from the user.
+	# this will return a list of Instantiated Trail Objects.
+
+	# Parse our JSON output
+	
+	#Uncomment this for live API version.  This is commented out so that we can save our API requests
+	
+	# request = requests.get(f'http://www.mtbproject.com/data/get-trails?lat={lat}&lon={lon}&maxDistance={maxDistance}&key={key}&maxResults=20')	
+	# trails_text = request.text
+	# trails_dict = json.loads(trails_text)
+
+	# Writing our api call data to a json file for continued use instead of making api requests.  Commented out because we don't want to updated
+	# our file and because our api isn't making requsts right now.
+	
+	# with open('trailsApiData.json', 'w') as json_file:
+	# 	json.dump(trails_dict, json_file)
+	
+	# Loading in our Trail Data test so we can play around with html without making api requests.
+	
+	with open('trailsBikeApiData.json') as f:
+		trails_dict = json.load(f)
+
+	trails = []
+
+	# grab out keys in the JSON output for each variable and store the key-values into our trails class.
+	# we will then store all of our classes as objects in our trails lists for later use.
+	for t in trails_dict['trails']:
+		name = t['name']
+		id = t['id']
+		conditionStatus = t['conditionStatus']
+		conditionDate = t['conditionDate']
+		try:
+			conditionDetails = t['conditionDetails'].lower()
+		except:
+			conditionDetails = t['conditionDetails']			
+		rating = t['stars']
+		lat = t['latitude']
+		lon = t['longitude']
+		difficulty = t['difficulty']
+		trail = BikeTrail(id=id, name=name, conditionStatus=conditionStatus, conditionDetails=conditionDetails, conditionDate=conditionDate,
 					rating=rating, lat=lat, lon=lon, difficulty=difficulty)
 		trails.append(trail)
 	
@@ -217,7 +323,7 @@ def compare_weather_to_trail_condition(trail, weath):
 	# given trail location and write an algorithm to determine the current condition.
 	
 	# trail_keys is a list of strings of the trail conditions
-	trail_keys = trail.get_condition_keywords()
+	trail_keys = trail.get_bike_condition_keywords()
 
 	# weather is a string of what the previous day precipitation was.  Should be a single string.
 	weather = weath.get_current_precip()
@@ -324,20 +430,35 @@ def get_bike():
 
 @app.route('/hike')
 def get_hike():
-	return render_template('hike.html')
+	states = get_states()
+	return render_template('hike.html', states=states)
 
-@app.route('/trails', methods=['POST'])
-def trail_search():
+@app.route('/bike_trails', methods=['POST'])
+def bike_trail_search():
 	user_address = create_address(request.form['inputRoadNum'], town=request.form['inputCity'], state=request.form['inputState'],
 					zipcode=request.form['inputZip'])
 	user_distance = request.form['Distance']
 	user_loc = get_coords(user_address, config.google_maps_key)
-	trail_list = get_trails(lat=user_loc.lat, lon=user_loc.lon, maxDistance=user_distance, key=config.mtb_api_key)
+	trail_list = get_bike_trails(lat=user_loc.lat, lon=user_loc.lon, maxDistance=user_distance, key=config.mtb_api_key)
 	for trail in trail_list:
 		trail_weather_json = get_location_data(lat=trail.lat, lon=trail.lon)
 		trail_weather = get_weather(trail_weather_json)
 		trail.conditions = compare_weather_to_trail_condition(trail, trail_weather)
-	return render_template('trails.html', trails=trail_list, user_loc=user_loc, user_distance=user_distance)
+	return render_template('bike_trails.html', trails=trail_list, user_loc=user_loc, user_distance=user_distance)
+
+@app.route('/hike_trails', methods=['POST'])
+def hike_trail_search():
+	user_address = create_address(request.form['inputRoadNum'], town=request.form['inputCity'], state=request.form['inputState'],
+					zipcode=request.form['inputZip'])
+	user_distance = request.form['Distance']
+	user_loc = get_coords(user_address, config.google_maps_key)
+	trail_list = get_hike_trails(lat=user_loc.lat, lon=user_loc.lon, maxDistance=user_distance, key=config.mtb_api_key)
+	for trail in trail_list:
+		trail_weather_json = get_location_data(lat=trail.lat, lon=trail.lon)
+		trail_weather = get_weather(trail_weather_json)
+		trail.conditions = compare_weather_to_trail_condition(trail, trail_weather)
+	return render_template('hike_trails.html', trails=trail_list, user_loc=user_loc, user_distance=user_distance)
+
 
 @app.route('/help')
 def get_help():
